37b8195a361ffec3b95b7ec69c10c00453b4dd07
Arrowfield <768449566@qq.com>
2020-05-10 11:29:27 +0800

    update

---
+++ src/components/Tags.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="tags">
+    <a v-waves href="#"><i class="iconfont iconrili"></i>2020年04月17日</a>
+    <a v-waves href="#"><i class="iconfont iconredu"></i>21888 °C</a>
+    <a v-waves href="#"><i class="iconfont iconxx"></i>270</a>
+    <a v-waves href="#"><i class="iconfont icondaohang1"></i>环境搭建</a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Tags"
+  }
+</script>
+
+<style lang="scss" scoped>
+
+  .tags {
+    margin: 10px 0;
+  }
+
+  .tags a {
+    display: inline-flex;
+    border-radius: 10px;
+    padding: 5px 10px;
+    color: #fff;
+    font-size: 14px;
+    margin-bottom: 5px;
+    margin-right: 10px;
+    overflow: hidden;
+    align-items: center;
+
+    i {
+      margin-right: 5px;
+      font-size: 14px;
+    }
+
+    &:nth-child(1) {
+      background: #ff4e6a;
+    }
+
+    &:nth-child(2) {
+      background: #ffaa73;
+    }
+
+    &:nth-child(3) {
+      background: #fed466;
+    }
+
+    &:nth-child(4) {
+      background: #3cdc82;
+    }
+
+    &:nth-child(5) {
+      background: #64dcf0;
+    }
+
+    &:nth-child(6) {
+      background: #64b9ff;
+    }
+  }
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,23 +1,84 @@
 <template>
   <div class="about-page">
-
+    <div class="img-cont">
+      <img src="https://img.hacpai.com/bing/20180919.jpg?imageView2/1/w/960/h/540/interlace/1/q/100" alt="">
+      <h1 class="title">关于我</h1>
+    </div>
+    <Tags/>
+    <div class="intro-detail">
+      <p>我是谁</p>
+      <ul>
+        <li>一个来自重庆山区的少年，喜欢唱、跳、rap。。。。。好像跑题了，重新来。</li>
+        <li>一个来自重庆山区的少年，即将结束大学生涯却拿不到毕业证的想成为程序员的码农。</li>
+        <li>金戋(jian)取自我名字中第二个字，学习docker时无意中发现了<a href="https://github.com/b3log" target="_blank">solo</a>，就随手搭建了一个，于是就有了<a href="http://www.jinjianh.com" target="_blank">墨殇的技术博客</a></li>
+        <li>一个刚步入社会，略带中二的少年，希望成为一名很优秀的程序员</li>
+      </ul>
+      <p>我在干嘛</p>
+      <ul>
+        <li>一个来自重庆山区的少年，喜欢唱、跳、rap。。。。。好像跑题了，重新来。</li>
+        <li>一个来自重庆山区的少年，即将结束大学生涯却拿不到毕业证的想成为程序员的码农。</li>
+        <li>金戋(jian)取自我名字中第二个字，学习docker时无意中发现了<a href="https://github.com/b3log" target="_blank">solo</a>，就随手搭建了一个，于是就有了<a href="http://www.jinjianh.com" target="_blank">墨殇的技术博客</a></li>
+        <li>一个刚步入社会，略带中二的少年，希望成为一名很优秀的程序员</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+  import Tags from "@/components/Tags";
+
   export default {
-    name: "About"
+    name: "About",
+    components: {
+      Tags
+    }
   }
 </script>
 
 <style lang="scss" scoped>
 
-  .about-page{
+  .about-page {
 
-    margin: 20px auto;
-    border-radius: 10px;
-    background-color: #fff;
-    padding: 20px 20px 0;
-    box-shadow: 0 0 1rem rgba(161, 177, 204, .4);
+    .img-cont {
+      overflow: hidden;
+      position: relative;
+      img {
+        border-radius: 10px;
+      }
+      .title {
+        position: absolute;
+        bottom: 0;
+        padding: 15px;
+        color: #fff;
+        font-size: 2em;
+        width: 100%;
+        font-weight: 400;
+        cursor: auto;
+        z-index: 5;
+      }
+    }
+
+    .intro-detail{
+      margin-bottom: 20px;
+      border-radius: 10px;
+      background-color: #fff;
+      padding: 20px;
+      box-shadow: 0 0 1rem rgba(161, 177, 204, .4);
+
+      p{
+        margin-bottom: 16px;
+      }
+
+      ul{
+        list-style: disc;
+        padding-left: 2em;
+        margin-bottom:16px;
+      }
+
+      li{
+        line-height: 24px;
+      }
+
+    }
   }
 </style>
