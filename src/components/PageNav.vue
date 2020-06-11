aa869a5da1ad33ea00c5aa2db59e472ad1d11eca
Arrowfield <768449566@qq.com>
2020-06-11 22:24:08 +0800

    update

--- src/components/PageNav.vue
+++ src/components/PageNav.vue
@@ -34,7 +34,13 @@
     methods: {
       prevPage() {
         let i = this.currPage - 1
-        if (this.currPage >= 6 && this.currPage < this.nums[this.nums.length - 3] - 2) {
+        if (this.currPage < 6) {
+          let start = [], end = this.nums.slice(-4)
+          for (let i = 1; i <= 6; i++) {
+            start.push(i)
+          }
+          this.nums = start.concat(end)
+        } else if (this.currPage >= 6 && this.currPage < this.nums[this.nums.length - 3] - 2) {
           let start = [1, "..."], end = this.nums.slice(-4)
           let center = [i - 2, i - 1, i, i + 1]
           this.nums = start.concat(center).concat(end)
@@ -54,11 +60,18 @@
       changePage(i) {
         if (typeof i === "number") {
           // console.log(this.nums[this.nums.length - 3] - 2)
-          if(i >= 6 && i < this.nums[this.nums.length - 3] - 1){
+          if (i < 6) {
+            let start = [], end = this.nums.slice(-4)
+            for (let i = 1; i <= 6; i++) {
+              start.push(i)
+            }
+            this.nums = start.concat(end)
+          } else if (i >= 6 && i < this.nums[this.nums.length - 3] - 1) {
             let start = [1, "..."], end = this.nums.slice(-4)
             let center = [i - 2, i - 1, i, i + 1]
             this.nums = start.concat(center).concat(end)
           }
+
           this.$emit("update:currPage", i)
           this.$emit('changeCurrPage', i)
 
