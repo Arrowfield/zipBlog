bb837654c856d648a5ba3b4bdf987ccb44eee801
field fang <fieldfang@fielddeMacBook-Air.local>
2021-11-01 22:19:03 +0800

    update path

--- src/router/index.js
+++ src/router/index.js
@@ -90,7 +90,7 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  mode: 'hash',
+  mode: 'history',
   linkActiveClass: 'active',
   base: process.env.BASE_URL,
   scrollBehavior (to, from, savedPosition) {
