58e473c4b50217ac8dbf91e10a0de683251691de
Arrowfield <768449566@qq.com>
2021-11-08 22:35:28 +0800

    update

--- src/router/greenBook.js
+++ src/router/greenBook.js
@@ -1,5 +1,7 @@
 export default [
-    {path: "/", component: () => import('@/views/skins/GreenBook/index.vue')},
-    {path: "/archive", component: () => import('@/views/skins/GreenBook/archives.vue')},
-    {path: "/download", component: () => import('@/views/skins/GreenBook/download.vue')}
+  {path: "/", component: () => import('@/views/skins/GreenBook/index.vue')},
+  {path: "/archive", component: () => import('@/views/skins/GreenBook/archives.vue')},
+  {path: "/download", component: () => import('@/views/skins/GreenBook/download.vue')},
+  {path: "/article-detail/:id", component: () => import('@/views/skins/GreenBook/article.vue')},
+
 ]
--- src/views/skins/GreenBook/article.vue
+++ src/views/skins/GreenBook/article.vue
@@ -1,13 +1,37 @@
 <template>
-    
+  <layout-slot class="article-main" class-name="article-content">
+    <div class="post-header-thumb  bg-orange" style="background-image:url(https://cdn.rawchen.com/2021/04/onedrive/00.jpg);">
+      <div class="post-header-thumb-op" style="background-image:url(https://cdn.rawchen.com/2021/04/onedrive/00.jpg);"></div>
+      <div class="post-header-thumb-cover">
+        <div class="post-header-thumb-container">
+          <div class="post-header-thumb-title">
+            6元做一个2.6m/s下行、5T存储的私人网盘			</div>
+          <div class="post-header-thumb-meta">
+            <time datetime="2021-04-04T16:09:00+08:00" itemprop="datePublished"><i class="fa fa-clock-o" aria-hidden="true"> </i> 2021-04-04</time>&nbsp;&nbsp; <i class="fa fa-tags" aria-hidden="true"></i> <a href="https://rawchen.com/category/enjoy/">资源分享</a>&nbsp;&nbsp; <i class="fa fa-comments-o" aria-hidden="true"></i> <a href="#comments"> 19 评论</a>&nbsp;&nbsp; <i class="fa fa-eye" aria-hidden="true"></i> 8629 浏览
+          </div>
+          <div class="post-tags">
+            <a href="https://rawchen.com/tag/OneDrive/">OneDrive</a>			</div>
+        </div>
+      </div>
+    </div>
+  </layout-slot>
 </template>
 
 <script>
-    export default {
-        name: "article"
-    }
+  import LayoutSlot from "./slot/layout-slot";
+
+  export default {
+    name: "vue-article",
+    components: {LayoutSlot}
+  }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  .article-main{}
+  // 针对整个文章的渲染
+</style>
+<style lang="scss">
+  .article-content{
+    max-width: 100%;
+  }
 </style>
